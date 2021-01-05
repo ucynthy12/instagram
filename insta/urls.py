158b0f3370cbbee50e3586e61bbfc10b2cfdf9d8
cynthia umutoniwabo <ucynthy12@gmail.com>
2021-01-05 23:00:54 +0200

    added the path and static files

--- insta/urls.py
+++ insta/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('instagram.urls'))
+    path('',include('instagram.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
